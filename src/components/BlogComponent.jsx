779977c3e9c36c15dd8143eba391e80331c9d579
Manisha Gajula <112008195+manishagajula@users.noreply.github.com>
2024-04-19 17:07:51 +0530

    added:files

--- src/components/BlogComponent.jsx
+++ src/components/BlogComponent.jsx
@@ -48,7 +48,8 @@ export const BlogComponent = () => {
       </div>
 
       <iframe
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3772.5273421048755!2d72.81686747536598!3d18.99647065446408!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7cff81471d8f7%3A0xfe0f743d6a385cef!2sSwan%20Capital!5e0!3m2!1sen!2sin!4v1713524822569!5m2!1sen!2sin"
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d37725273421048755!2d7281686747536598!3d1899647065446408!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7cff81471d8f7%3A0xfe0f743d6a385cef!2sSwan%20Capital!5e0!3m2!1sen!2sin!4v1713524822569!5m2!1sen!2sin"
+        title="examplemap"
         width="100%"
         height="450"
         allowfullscreen=""
